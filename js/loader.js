e4661a62db10393a76c3c976630e580971db8872
Michele Gallo <michelegallo997@gmail.com>
2024-10-05 16:51:45 -0300

    portafolio.1.9

--- js/loader.js
+++ js/loader.js
@@ -1,14 +1,7 @@
-// Escucha el evento 'load' que se dispara cuando toda la página se ha cargado
+// Loader
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
-const backgroundMusic = document.getElementById("background-music");
-backgroundMusic.volume = 0.2;
-// Reproducir la música
-backgroundMusic.play();
-
-// Pausar la música
-backgroundMusic.pause();
 
 window.addEventListener("load", function() {
     // Selecciona el contenedor del loader
@@ -20,5 +13,25 @@ window.addEventListener("load", function() {
     // Elimina el loader del DOM después de la transición
     setTimeout(() => {
         loaderContainer.style.display = "none";
-    },3500); // Ajusta el tiempo para coincidir con la duración de la transición
+    }, 3500); // Ajusta el tiempo para coincidir con la duración de la transición
+});
+
+// Control de volumen
+const backgroundMusic = document.getElementById("background-music");
+const volumeToggle = document.getElementById("volume-toggle");
+const volumeIcon = document.getElementById("volume-icon");
+
+// Reproducción automática al cargar
+backgroundMusic.volume = 0.5;
+backgroundMusic.play();
+
+// Alternar el volumen al hacer clic en el icono
+volumeToggle.addEventListener("click", function () {
+    if (backgroundMusic.muted) {
+        backgroundMusic.muted = false;
+        volumeIcon.classList.replace("fa-volume-off", "fa-volume-up");
+    } else {
+        backgroundMusic.muted = true;
+        volumeIcon.classList.replace("fa-volume-up", "fa-volume-off");
+    }
 });
