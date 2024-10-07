cae36867fbdce5bfb3fa8d743f2c02d05231d4d6
Michele Gallo <michelegallo997@gmail.com>
2024-10-06 21:36:19 -0300

    portafolio.5.3

--- js/loader.js
+++ js/loader.js
@@ -47,3 +47,40 @@ document.getElementById('refresh-button').addEventListener('click', function() {
     location.reload(); // Refresca la página
 });
 
+
+
+
+
+// Seleccionar elementos relevantes del carrusel
+const carouselTrack = document.querySelector('.carousel-track');
+const cards = Array.from(carouselTrack.children);
+const prevButton = document.querySelector('.carousel-control.prev');
+const nextButton = document.querySelector('.carousel-control.next');
+
+// Función para mover el carrusel a la izquierda
+function moveToPrev() {
+    const lastCard = cards.pop(); // Elimina la última tarjeta del array
+    cards.unshift(lastCard); // Inserta la última tarjeta al inicio del array
+    carouselTrack.insertBefore(lastCard, carouselTrack.firstChild); // Mueve la tarjeta al frente visualmente
+}
+
+// Función para mover el carrusel a la derecha
+function moveToNext() {
+    const firstCard = cards.shift(); // Elimina la primera tarjeta del array
+    cards.push(firstCard); // Añade la primera tarjeta al final del array
+    carouselTrack.appendChild(firstCard); // Mueve la tarjeta al final visualmente
+}
+
+// Eventos de click para las flechas
+prevButton.addEventListener('click', moveToPrev);
+nextButton.addEventListener('click', moveToNext);
+
+
+const menuButton = document.querySelector('button[aria-controls="mobile-menu"]');
+const mobileMenu = document.getElementById('mobile-menu');
+
+// Agregar un evento al hacer clic en el botón
+menuButton.addEventListener('click', () => {
+  // Alternar la clase 'hidden' para mostrar u ocultar el menú
+  mobileMenu.classList.toggle('hidden');
+});
