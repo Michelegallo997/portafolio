47a8393376302b6e55edde4f685d714b921c3d35
Michele Gallo <michelegallo997@gmail.com>
2024-10-18 20:22:46 -0300

    portafolio3d_4

--- js/app.js
+++ js/app.js
@@ -259,11 +259,11 @@ const adjustForOrientation = () => {
     if (isMobile && isLandscape) {
         // Si el dispositivo es móvil y está en modo horizontal
         camera1.fov = 20; // Ajustamos el campo de visión (fov) para vista horizontal
-        camera1.position.z = 55; // Alejamos la cámara para que no se vea desconfigurado
+        camera1.position.z = 45; // Alejamos la cámara para que no se vea desconfigurado
     } else if (isMobile && !isLandscape) {
         // Si el dispositivo es móvil y está en modo vertical
         camera1.fov = 20;  
-        camera1.position.z = 50;
+        camera1.position.z = 40;
     } else {
         // Para dispositivos que no son móviles o resoluciones más grandes
         camera1.fov = 11; // Campo de visión original para pantallas más grandes
