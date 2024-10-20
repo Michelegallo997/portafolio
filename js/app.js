8d0bef4623bae4f1c003b79967b37bcb754af83f
Michele Gallo <michelegallo997@gmail.com>
2024-10-19 21:49:16 -0300

    portafolio3d_27

--- js/app.js
+++ js/app.js
@@ -252,19 +252,17 @@ window.addEventListener('resize', () => {
 
 
 // *** AJUSTES PARA DISPOSITIVOS MÓVILES Y CAMBIO DE ORIENTACIÓN ***
-
-// Función para ajustar el modelo 3D en dispositivos móviles cuando cambia la orientación
 const adjustForOrientation = () => {
     const isLandscape = window.matchMedia("(orientation: landscape)").matches; // Detectar si está en modo horizontal
     const isMobile = window.innerWidth <= 768; // Consideramos que es móvil si el ancho es menor o igual a 768px
 
     if (isMobile && isLandscape) {
         // Si el dispositivo es móvil y está en modo horizontal
-        camera1.fov = 30;  // Ajustamos el campo de visión (fov) para vista horizontal
+        camera1.fov = 20;  // Ajustamos el campo de visión (fov) para vista horizontal
         camera1.position.z = 55;  // Ajustamos la cámara más lejana
     } else if (isMobile && !isLandscape) {
         // Si el dispositivo es móvil y está en modo vertical
-        camera1.fov = 16;  
+        camera1.fov = 23;  
         camera1.position.z = 63;  // Ajuste moderado para vista vertical
     } else {
         // Para dispositivos que no son móviles o resoluciones más grandes
@@ -276,16 +274,9 @@ const adjustForOrientation = () => {
     renderer1.setSize(window.innerWidth, window.innerHeight);
 };
 
-// Evento para detectar cambios de orientación (horizontal/vertical)
-window.addEventListener('resize', () => {
-    adjustForOrientation();
-});
-
-// Llamada inicial para asegurarse de que el ajuste se aplique según la orientación actual
-adjustForOrientation();
-
-// Detectar el cambio de orientación y recargar la página cuando sea necesario
+// Detectar el cambio de orientación y actualizar solo el modelo
 window.addEventListener('orientationchange', function() {
-    // Recarga la página cuando cambie la orientación
-    location.reload();
+    // Solo actualiza el modelo 3D y la cámara en vez de recargar toda la página
+    adjustForOrientation();
+    reRender3D(); // Llamamos a la función de re-renderizado del modelo 3D
 });
